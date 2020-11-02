b9b44139b956fa12d09943672851d99e5a2cb1c1
Viraj <viraj@zmessenger.mobi>
2020-11-02 22:34:51 +0530

    getOrganizations api completed

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,7 +9,37 @@ export class AppService {
 
   async getOrganizations(@Req() request: Request) {
     try {
-      return organizations;
+      let organizationsData = [];
+      for (let organization of organizations) {
+        let ticketsData = [];
+        let usersData = [];
+        for (let ticket of tickets) {
+          if(organization._id===ticket.organization_id){
+            let obj = {
+              "ticket_id": ticket._id,
+              "ticket_subject": ticket.subject
+            }
+            ticketsData.push(obj);
+          }
+        }
+        for (let user of users) {
+          if(organization._id===user.organization_id){
+            let obj = {
+              "user_id": user._id,
+              "user_name": user.name
+            }
+            usersData.push(obj);
+          }
+        }
+        let organizationData = {
+          "_id": organization._id,
+          "name": organization.name,
+          "ticketsData":ticketsData,
+          "usersData":usersData
+        }        
+        organizationsData.push(organizationData);
+      }
+      return organizationsData;
     } catch (error) {
       throw new HttpException(error, error.status);
     }
